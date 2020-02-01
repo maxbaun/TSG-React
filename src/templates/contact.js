b1c2bf204403db6335373c2f629f7ba8b956c51a
Max Baun <max@d3applications.com>
2020-02-01 15:44:07 -0500

    fixing contact page query

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -14,12 +14,11 @@ export default class ContactTemplate extends React.Component {
 	static propTypes = {
 		data: PropTypes.object.isRequired,
 		location: PropTypes.object.isRequired,
-		site: PropTypes.object,
-		options: PropTypes.object.isRequired
+		site: PropTypes.object
 	};
 
 	render() {
-		const {currentPage} = this.props.data;
+		const {currentPage, options: {options}} = this.props.data;
 
 		return (
 			<Page contain padding={false}>
@@ -33,34 +32,34 @@ export default class ContactTemplate extends React.Component {
 							<h3>Connect</h3>
 							<div className={CSS.socialIcons}>
 								<SocialIcons
-									facebook={this.props.options.social.facebook}
-									pinterest={this.props.options.social.pinterest}
-									instagram={this.props.options.social.instagram}
-									vimeo={this.props.options.social.vimeo}
-									youtube={this.props.options.social.youtube}
+									facebook={options.social.facebook}
+									pinterest={options.social.pinterest}
+									instagram={options.social.instagram}
+									vimeo={options.social.vimeo}
+									youtube={options.social.youtube}
 								/>
 							</div>
 						</div>
 						<div className={CSS.sidebarBlock}>
 							<h3>Find Us</h3>
-							<p>{this.props.options.findUsMessage}</p>
+							<p>{options.findUsMessage}</p>
 							<ul className={CSS.find}>
 								<li>
 									<span className="fa fa-phone"/>
-									<a href={phoneLink(this.props.options.phone)}>{this.props.options.phone}</a>
+									<a href={phoneLink(options.phone)}>{options.phone}</a>
 								</li>
 								<li>
 									<span className="fa fa-fax"/>
-									<a href={emailLink(this.props.options.email)}>{this.props.options.fax}</a>
+									<a href={emailLink(options.email)}>{options.fax}</a>
 								</li>
 								<li>
 									<span className="fa fa-envelope"/>
-									<a href={emailLink(this.props.options.email)}>{this.props.options.email}</a>
+									<a href={emailLink(options.email)}>{options.email}</a>
 								</li>
 								<li>
 									<span className="fa fa-map-marker-alt"/>
 									{/* eslint-disable-next-line react/no-danger */}
-									<span dangerouslySetInnerHTML={innerHtml(this.props.options.address)}/>
+									<span dangerouslySetInnerHTML={innerHtml(options.address)}/>
 								</li>
 							</ul>
 						</div>
@@ -77,6 +76,22 @@ export const contactQuery = graphql`
 	query contactQuery($id: String!) {
 		currentPage: wordpressPage(id: {eq: $id}) {
 			...Page
+		}
+		options: wordpressAcfOptions {
+			options {
+				phone: contactPhoneNumber
+				email: contactEmail
+				fax: contactFax
+				address: contactAddress
+				findUsMessage: contactFindUsMessage
+				social: contactSocial {
+					facebook
+					pinterest
+					instagram
+					vimeo
+					youtube
+				}
+			}
 		}
 	}
 `;
