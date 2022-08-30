7efaaaae966354486f92caf56f425658df1a6017
Max Baun <max@d3applications.com>
2022-08-30 11:48:54 -0400

    not importing fragments

--- src/templates/vendors.js
+++ src/templates/vendors.js
@@ -12,172 +12,175 @@ import Seo from '../components/seo';
 import CSS from '../css/modules/vendors.module.scss';
 
 export default class VendorsTemplate extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.getVendorsInCategory = this.getVendorsInCategory.bind(this);
-		this.getCategoryById = this.getCategoryById.bind(this);
-	}
+    this.getVendorsInCategory = this.getVendorsInCategory.bind(this);
+    this.getCategoryById = this.getCategoryById.bind(this);
+  }
 
-	static propTypes = {
-		data: PropTypes.object.isRequired,
-		location: PropTypes.object.isRequired,
-		site: PropTypes.object
-	};
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    site: PropTypes.object
+  };
 
-	getVendorsInCategory(category) {
-		const {vendors} = this.props.data;
+  getVendorsInCategory(category) {
+    const {vendors} = this.props.data;
 
-		return vendors.edges
-			.filter(i => {
-				const {node: vendor} = i;
+    return vendors.edges
+      .filter(i => {
+        const {node: vendor} = i;
 
-				return vendor.vendorcategory[0] === category;
-			})
-			.map(v => v.node);
-	}
+        return vendor.vendorcategory[0] === category;
+      })
+      .map(v => v.node);
+  }
 
-	getCategoryById(id) {
-		const {categories} = this.props.data;
-		const found = categories.edges.find(c => c.node.wordpress_id === id);
+  getCategoryById(id) {
+    const {categories} = this.props.data;
+    const found = categories.edges.find(c => c.node.wordpress_id === id);
 
-		if (!found) {
-			return;
-		}
+    if (!found) {
+      return;
+    }
 
-		return found.node;
-	}
+    return found.node;
+  }
 
-	render() {
-		const {currentPage, options: {options}} = this.props.data;
+  render() {
+    const {
+      currentPage,
+      options: {options}
+    } = this.props.data;
 
-		const vendorCats = options.vendorCategories.map(c => c.category[0]);
-		const vendorTitle = `Boston's Most Trusted Wedding Vendors`;
+    const vendorCats = options.vendorCategories.map(c => c.category[0]);
+    const vendorTitle = `Boston's Most Trusted Wedding Vendors`;
 
-		return (
-			<PageWrap contain>
-				<Seo currentPage={currentPage} site={this.props.site} location={this.props.location}/>
-				<FlexibleContent page={currentPage}/>
-				<div className={CSS.categories}>
-					<h3>{vendorTitle}</h3>
-					<ul>
-						{vendorCats.map(c => {
-							const category = this.getCategoryById(c);
+    return (
+      <PageWrap contain>
+        <Seo currentPage={currentPage} site={this.props.site} location={this.props.location} />
+        <FlexibleContent page={currentPage} />
+        <div className={CSS.categories}>
+          <h3>{vendorTitle}</h3>
+          <ul>
+            {vendorCats.map(c => {
+              const category = this.getCategoryById(c);
 
-							return (
-								<li key={c}>
-									<ScrollTo target={`#vendorSection${c}`}>
-										{/* eslint-disable-next-line react/no-danger */}
-										<p dangerouslySetInnerHTML={innerHtml(category.name)}/>
-									</ScrollTo>
-								</li>
-							);
-						})}
-					</ul>
-				</div>
-				<div className={CSS.vendors}>
-					{vendorCats.map(categoryId => {
-						const category = this.getCategoryById(categoryId);
-						const vendors = this.getVendorsInCategory(categoryId);
+              return (
+                <li key={c}>
+                  <ScrollTo target={`#vendorSection${c}`}>
+                    {/* eslint-disable-next-line react/no-danger */}
+                    <p dangerouslySetInnerHTML={innerHtml(category.name)} />
+                  </ScrollTo>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={CSS.vendors}>
+          {vendorCats.map(categoryId => {
+            const category = this.getCategoryById(categoryId);
+            const vendors = this.getVendorsInCategory(categoryId);
 
-						return (
-							<div key={categoryId} id={`vendorSection${categoryId}`} className={CSS.vendorSection}>
-								{/* eslint-disable-next-line react/no-danger */}
-								<h3 dangerouslySetInnerHTML={innerHtml(category.name)}/>
-								<ul>
-									{vendors.map(vendor => {
-										return <li key={vendor.wordpress_id}>{this.renderVendor(vendor)}</li>;
-									})}
-								</ul>
-							</div>
-						);
-					})}
-				</div>
-			</PageWrap>
-		);
-	}
+            return (
+              <div key={categoryId} id={`vendorSection${categoryId}`} className={CSS.vendorSection}>
+                {/* eslint-disable-next-line react/no-danger */}
+                <h3 dangerouslySetInnerHTML={innerHtml(category.name)} />
+                <ul>
+                  {vendors.map(vendor => {
+                    return <li key={vendor.wordpress_id}>{this.renderVendor(vendor)}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </PageWrap>
+    );
+  }
 
-	renderVendor(vendor) {
-		const {title} = vendor;
-		const {phone, email, link, venuePage} = vendor.acf;
-		return (
-			<div className={CSS.vendor}>
-				{link ? (
-					<a href={link} target="_blank" rel="noopener noreferrer">
-						<h5
-							// eslint-disable-next-line react/no-danger
-							dangerouslySetInnerHTML={innerHtml(title)}
-						/>
-					</a>
-				) : (
-					<h5
-						// eslint-disable-next-line react/no-danger
-						dangerouslySetInnerHTML={innerHtml(title)}
-					/>
-				)}
-				<ul className={CSS.vendorDetails}>
-					{phone ? (
-						<li>
-							<span className="fa fa-phone"/>
-							<a href={phoneLink(phone)}>{phone}</a>
-						</li>
-					) : null}
-					{email ? (
-						<li>
-							<span className="fa fa-envelope"/>
-							<a href={emailLink(email)}>{email}</a>
-						</li>
-					) : null}
-					{link ? (
-						<li>
-							<span className="fa fa-keyboard"/>
-							<a href={link} target="_blank" rel="noopener noreferrer">
-								{link}
-							</a>
-						</li>
-					) : null}
-					{venuePage && venuePage.url ? (
-						<li style={{marginTop: 10}}>
-							<Button to={venuePage.url} size="xs" style={{display: 'block', width: 105, boxShadow: 'none'}}>
-								See Photos
-							</Button>
-						</li>
-					) : null}
-				</ul>
-			</div>
-		);
-	}
+  renderVendor(vendor) {
+    const {title} = vendor;
+    const {phone, email, link, venuePage} = vendor.acf;
+    return (
+      <div className={CSS.vendor}>
+        {link ? (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <h5
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={innerHtml(title)}
+            />
+          </a>
+        ) : (
+          <h5
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={innerHtml(title)}
+          />
+        )}
+        <ul className={CSS.vendorDetails}>
+          {phone ? (
+            <li>
+              <span className="fa fa-phone" />
+              <a href={phoneLink(phone)}>{phone}</a>
+            </li>
+          ) : null}
+          {email ? (
+            <li>
+              <span className="fa fa-envelope" />
+              <a href={emailLink(email)}>{email}</a>
+            </li>
+          ) : null}
+          {link ? (
+            <li>
+              <span className="fa fa-keyboard" />
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                {link}
+              </a>
+            </li>
+          ) : null}
+          {venuePage && venuePage.url ? (
+            <li style={{marginTop: 10}}>
+              <Button to={venuePage.url} size="xs" style={{display: 'block', width: 105, boxShadow: 'none'}}>
+                See Photos
+              </Button>
+            </li>
+          ) : null}
+        </ul>
+      </div>
+    );
+  }
 }
 
-import {Page, Vendor} from '../utils/fragments'; // eslint-disable-line no-unused-vars
+// import {Page, Vendor} from '../utils/fragments'; // eslint-disable-line no-unused-vars
 
 export const pageQuery = graphql`
-	query vendorsTemplateQuery($id: String!) {
-		currentPage: wordpressPage(id: {eq: $id}) {
-			...Page
-		}
-		categories: allWordpressWpVendorcategory {
-			edges {
-				node {
-					wordpress_id
-					name
-					count
-				}
-			}
-		}
-		vendors: allWordpressWpVendor {
-			edges {
-				node {
-					...Vendor
-				}
-			}
-		}
-		options: wordpressAcfOptions {
-			options {
-				vendorCategories {
-					category
-				}
-			}
-		}
-	}
+  query vendorsTemplateQuery($id: String!) {
+    currentPage: wordpressPage(id: {eq: $id}) {
+      ...Page
+    }
+    categories: allWordpressWpVendorcategory {
+      edges {
+        node {
+          wordpress_id
+          name
+          count
+        }
+      }
+    }
+    vendors: allWordpressWpVendor {
+      edges {
+        node {
+          ...Vendor
+        }
+      }
+    }
+    options: wordpressAcfOptions {
+      options {
+        vendorCategories {
+          category
+        }
+      }
+    }
+  }
 `;
--- src/templates/venues.js
+++ src/templates/venues.js
@@ -10,119 +10,124 @@ import CSS from '../css/modules/venues.module.scss';
 import {innerHtml} from '../utils/wordpressHelpers';
 
 export default class PageTemplate extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			query: ''
-		};
+    this.state = {
+      query: ''
+    };
 
-		this.handleSearchQuery = this.handleSearchQuery.bind(this);
-	}
+    this.handleSearchQuery = this.handleSearchQuery.bind(this);
+  }
 
-	static propTypes = {
-		data: PropTypes.object.isRequired,
-		location: PropTypes.object.isRequired,
-		site: PropTypes.object
-	};
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    site: PropTypes.object
+  };
 
-	transformVenues() {
-		const {query} = this.state;
-		const {venues: venueData} = this.props.data;
+  transformVenues() {
+    const {query} = this.state;
+    const {venues: venueData} = this.props.data;
 
-		if (!venueData || !venueData.edges) {
-			return [];
-		}
+    if (!venueData || !venueData.edges) {
+      return [];
+    }
 
-		// Pull out the nodes
-		const venueNodes = venueData.edges.map(v => v.node);
+    // Pull out the nodes
+    const venueNodes = venueData.edges.map(v => v.node);
 
-		// Filter out the venues vased on the search query
-		const filteredVenues = venueNodes.filter(v => {
-			const title = v.title.toLowerCase();
-			const location = v.location.toLowerCase();
+    // Filter out the venues vased on the search query
+    const filteredVenues = venueNodes.filter(v => {
+      const title = v.title.toLowerCase();
+      const location = v.location.toLowerCase();
 
-			return title.includes(query) || location.includes(query);
-		});
+      return title.includes(query) || location.includes(query);
+    });
 
-		// Sort the venues alphabetically
-		const sortedVenues = filteredVenues.sort((a, b) => {
-			const aTitle = a.title.toLowerCase();
-			const bTitle = b.title.toLowerCase();
+    // Sort the venues alphabetically
+    const sortedVenues = filteredVenues.sort((a, b) => {
+      const aTitle = a.title.toLowerCase();
+      const bTitle = b.title.toLowerCase();
 
-			if (aTitle < bTitle) {
-				return -1;
-			}
+      if (aTitle < bTitle) {
+        return -1;
+      }
 
-			if (aTitle > bTitle) {
-				return 1;
-			}
+      if (aTitle > bTitle) {
+        return 1;
+      }
 
-			return 0;
-		});
+      return 0;
+    });
 
-		return sortedVenues;
-	}
+    return sortedVenues;
+  }
 
-	handleSearchQuery(e) {
-		const query = e.target.value || '';
+  handleSearchQuery(e) {
+    const query = e.target.value || '';
 
-		this.setState({query: query.toLowerCase()});
-	}
+    this.setState({query: query.toLowerCase()});
+  }
 
-	render() {
-		const {currentPage} = this.props.data;
-		const venues = this.transformVenues();
+  render() {
+    const {currentPage} = this.props.data;
+    const venues = this.transformVenues();
 
-		return (
-			<DefaultLayout>
-				<div>
-					<Seo currentPage={currentPage} site={this.props.site} location={this.props.location}/>
-					<FlexibleContent page={currentPage}/>
-					<div className={CSS.searchWrap}>
-						<div className={CSS.searchInput}>
-							<input type="text" onChange={this.handleSearchQuery} value={this.state.query} placeholder="Search Venues..."/>
-						</div>
-					</div>
-					<div className={CSS.venues}>
-						<ul>
-							{venues.map(venue => {
-								return (
-									<li key={venue.wordpress_id}>
-										<Link to={`/venue/${venue.slug}`} classname={CSS.venue}>
-											<div className={CSS.venueImage}>
-												<Image image={venue.image.url} style={{height: '100%'}} size="medium_large"/>
-											</div>
-											<div className={CSS.venueTitle}>
-												{/* eslint-disable-next-line react/no-danger */}
-												<h3 dangerouslySetInnerHTML={innerHtml(venue.title)}/>
-											</div>
-										</Link>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-				</div>
-			</DefaultLayout>
-		);
-	}
+    return (
+      <DefaultLayout>
+        <div>
+          <Seo currentPage={currentPage} site={this.props.site} location={this.props.location} />
+          <FlexibleContent page={currentPage} />
+          <div className={CSS.searchWrap}>
+            <div className={CSS.searchInput}>
+              <input
+                type="text"
+                onChange={this.handleSearchQuery}
+                value={this.state.query}
+                placeholder="Search Venues..."
+              />
+            </div>
+          </div>
+          <div className={CSS.venues}>
+            <ul>
+              {venues.map(venue => {
+                return (
+                  <li key={venue.wordpress_id}>
+                    <Link to={`/venue/${venue.slug}`} classname={CSS.venue}>
+                      <div className={CSS.venueImage}>
+                        <Image image={venue.image.url} style={{height: '100%'}} size="medium_large" />
+                      </div>
+                      <div className={CSS.venueTitle}>
+                        {/* eslint-disable-next-line react/no-danger */}
+                        <h3 dangerouslySetInnerHTML={innerHtml(venue.title)} />
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </DefaultLayout>
+    );
+  }
 }
 
-import {Page, Venue} from '../utils/fragments'; // eslint-disable-line no-unused-vars
+// import {Page, Venue} from '../utils/fragments'; // eslint-disable-line no-unused-vars
 import DefaultLayout from '../components/layout';
 
 export const pageQuery = graphql`
-	query venueTemplateQuery($id: String!) {
-		currentPage: wordpressPage(id: {eq: $id}) {
-			...Page
-		}
-		venues: allWordpressTsgVenues(filter: {slug: {ne: "do-not-delete"}}) {
-			edges {
-				node {
-					...Venue
-				}
-			}
-		}
-	}
+  query venueTemplateQuery($id: String!) {
+    currentPage: wordpressPage(id: {eq: $id}) {
+      ...Page
+    }
+    venues: allWordpressTsgVenues(filter: {slug: {ne: "do-not-delete"}}) {
+      edges {
+        node {
+          ...Venue
+        }
+      }
+    }
+  }
 `;
