c871ee4072d127a92df7163126d8efaf57122874
Max Baun <max@d3applications.com>
2023-12-12 16:25:13 -0500

    adding instagram embed js

--- src/html.js
+++ src/html.js
@@ -14,48 +14,50 @@ let stylesStr;
 // }
 
 export default class Html extends React.Component {
-	static propTypes = {
-		headComponents: PropTypes.node.isRequired,
-		body: PropTypes.node.isRequired,
-		postBodyComponents: PropTypes.node.isRequired
-	};
+  static propTypes = {
+    headComponents: PropTypes.node.isRequired,
+    body: PropTypes.node.isRequired,
+    postBodyComponents: PropTypes.node.isRequired
+  };
 
-	render() {
-		// Let css;
-		// if (process.env.NODE_ENV === `production`) {
-		// 	css = (
-		// 		<style
-		// 			id="gatsby-inlined-css"
-		// 			dangerouslySetInnerHTML={{__html: stylesStr}} // eslint-disable-line
-		// 		/>
-		// 	);
-		// }
+  render() {
+    // Let css;
+    // if (process.env.NODE_ENV === `production`) {
+    // 	css = (
+    // 		<style
+    // 			id="gatsby-inlined-css"
+    // 			dangerouslySetInnerHTML={{__html: stylesStr}} // eslint-disable-line
+    // 		/>
+    // 	);
+    // }
 
-		return (
-			<html op="news" lang="en">
-				<head>
-					{this.props.headComponents}
-					<meta name="referrer" content="origin"/>
-					<meta charSet="utf-8"/>
-					<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, user-scalable=no"/>
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-					<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700" rel="stylesheet"/>
-					{/* <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet"/>
+    return (
+      <html op="news" lang="en">
+        <head>
+          {this.props.headComponents}
+          <meta name="referrer" content="origin" />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, user-scalable=no" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet"/>
 					<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC" rel="stylesheet"/> */}
 
-					<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&display=swap" rel="stylesheet" />
 
-					<script src="https://tsgtools.com/check_req_info_form.js"/>
-					{/* {css} */}
-				</head>
-				<body data-theme="default">
-					<div
-						id="___gatsby"
-						dangerouslySetInnerHTML={{__html: this.props.body}} //eslint-disable-line
-					/>
-					{this.props.postBodyComponents}
-				</body>
-			</html>
-		);
-	}
+          <script src="https://tsgtools.com/check_req_info_form.js" />
+          {/* {css} */}
+        </head>
+        <body data-theme="default">
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{__html: this.props.body}} //eslint-disable-line
+          />
+          {this.props.postBodyComponents}
+
+          <script async src="//www.instagram.com/embed.js" />
+        </body>
+      </html>
+    );
+  }
 }
